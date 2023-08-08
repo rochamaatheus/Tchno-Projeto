a2c9452c56538078b7bc46fb66d2f36c1b9c0dc9
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-08-08 19:02:46 -0300

    Estrutura de fetch dos produtos

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const vm = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    produtos: [],
+  },
+  methods: {
+    fetchProdutos() {
+      fetch('./api/produtos.json')
+        .then((r) => r.json())
+        .then((json) => {
+          this.produtos = json;
+        });
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
 });
